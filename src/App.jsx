4a051249c1006256645a9850e90c449e2ce34e3d
jhraju <jhraju890@gmail.com>
2024-11-02 16:04:16 +0600

    added map solve and add coffee

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
  
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
-
-function App() {
+import CoffeeCard from './components/CoffeeCard';
+import { useEffect, useState } from 'react';
+import { data } from 'autoprefixer';
  
-      const coffees = useLoaderData();
+function App() {
+    
+  const [coffee, setCoffee] = useState();
+      useEffect(() =>{
+        fetch('http://localhost:5000/coffee')
+        .then(res =>res.json())
+        .then(data => setCoffee(data));
+      })
+      const coffees = useLoaderData({coffee});
 
   return (
-    <>
+    <div>
       <h1 className='text-6xl text-purple-600'>Hot cold coffee {coffees.length}</h1>
-    </>
+    
+    <div>
+    {
+      coffee?.map (coffee => <CoffeeCard key={coffee} coffee={coffee}></CoffeeCard>)
+     }
+     
+    </div>
+     
+      
+    </div>
   )
 }
 
